f514923a8c33c3a84426494f255277c7f03b4d4c
nukkuminen <90336027+oldschoolotaku@users.noreply.github.com>
2025-12-11 19:53:16 +0300

    Ported event icons from Next

---
+++ Content.Client/_CorvaxGoob/EventIcon/ShowEventIconSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared._CorvaxGoob.EventIcon;
+using Content.Shared.StatusIcon.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client._CorvaxGoob.EventIcon;
+
+public sealed class ShowEventIconSystem : EntitySystem
+{
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<EventIconComponent, GetStatusIconsEvent>(GetEventIcons);
+    }
+
+    private void GetEventIcons(Entity<EventIconComponent> ent, ref GetStatusIconsEvent args)
+    {
+        var iconPrototype = _prototype.Index(ent.Comp.StatusIcon);
+        args.StatusIcons.Add(iconPrototype);
+    }
+}
