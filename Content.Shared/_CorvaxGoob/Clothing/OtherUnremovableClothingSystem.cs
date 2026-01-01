fb32f061e0bc6e956c1af764f21b6237424be1c3
JrInventor05Next <205915704+JrInventor05Next@users.noreply.github.com>
2026-01-01 14:23:01 +0700

    Create system

---
+++ Content.Shared/_CorvaxGoob/Clothing/OtherUnremovableClothingSystem.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Examine;
+using Content.Shared.Inventory;
+using Content.Shared.Inventory.Events;
+
+namespace Content.Shared._CorvaxGoob.Clothing;
+
+/// <summary>
+///     A system for the operation of a component that prohibits the others player from taking off his own clothes that have this component.
+/// </summary>
+public sealed class OtherUnremovableClothingSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<OtherUnremovableClothingComponent, BeingUnequippedAttemptEvent>(OnUnequip);
+        SubscribeLocalEvent<OtherUnremovableClothingComponent, ExaminedEvent>(OnUnequipMarkup);
+    }
+
+    private void OnUnequip(Entity<OtherUnremovableClothingComponent> otherUnremovableClothing, ref BeingUnequippedAttemptEvent args)
+    {
+        if (TryComp<ClothingComponent>(otherUnremovableClothing, out var clothing) && (clothing.Slots & args.SlotFlags) == SlotFlags.NONE)
+            return;
+
+        if (args.UnEquipTarget != args.Unequipee)
+        {
+            args.Cancel();
+        }
+    }
+
+    private void OnUnequipMarkup(Entity<OtherUnremovableClothingComponent> otherUnremovableClothing, ref ExaminedEvent args)
+    {
+        args.PushMarkup(Loc.GetString("comp-other-unremovable-clothing"));
+    }
+}
