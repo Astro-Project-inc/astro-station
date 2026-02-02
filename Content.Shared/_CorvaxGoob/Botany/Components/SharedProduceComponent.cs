0a3f1eb8d93c97d83b2051642b5b2c0be0b57736
KAVALDi <kavaldi.ss14@gmail.com>
2026-02-02 16:50:24 +0700

    удалил файлы что не несут функционала

--- Content.Shared/_CorvaxGoob/Botany/Components/SharedProduceComponent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._CorvaxGoob.Botany.Components;
-
-[NetworkedComponent]
-public abstract partial class SharedProduceComponent : Component
-{
-}
